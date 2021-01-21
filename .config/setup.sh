5228ed5b1740b8628ab59fb4029c6e5ad2fa0bc2
00Nave198 <00evanmasters@gmail.com>
2021-01-20 19:35:43 -0500

    updated setup

--- .config/setup.sh
+++ .config/setup.sh
@@ -4,11 +4,11 @@ git clone --bare https://github.com/00Nave198/dotfiles.git $HOME/.dotfiles
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .dotfiles-backup
 dotfiles checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
+    mkdir -p .dotfiles-backup
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
